d8e50a393a688bcd53fb36d8a0890ac7ad84e398
brownmaple <krishnavaneeth.n@gmail.com>
2024-02-04 11:38:35 -0500

    change material status after reservation/unreservation
    This ofcourse needs transaction management in future

--- src/main/java/com/dxbpoc/inventory/serviceImpl/JobServiceImpl.java
+++ src/main/java/com/dxbpoc/inventory/serviceImpl/JobServiceImpl.java
@@ -3,7 +3,9 @@ package com.dxbpoc.inventory.serviceImpl;
 import com.dxbpoc.inventory.dto.JobDto;
 import com.dxbpoc.inventory.dto.JobResponse;
 import com.dxbpoc.inventory.entity.Job;
+import com.dxbpoc.inventory.entity.Material;
 import com.dxbpoc.inventory.repository.JobRepository;
+import com.dxbpoc.inventory.repository.MaterialRepo;
 import com.dxbpoc.inventory.service.JobService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,6 +26,9 @@ public class JobServiceImpl implements JobService {
 
     @Autowired
     JobRepository jobRepo;
+
+    @Autowired
+    MaterialRepo materialRepo;
 
     @Bean
     public ModelMapper modelMapper() {
@@ -40,6 +47,13 @@ public class JobServiceImpl implements JobService {
         Job job = modelMapper().map(jobDto, Job.class);
         job.setStatus("Reserved");
         Job savedJob = jobRepo.save(job);
+
+        Optional<Material> matById = materialRepo.findById(jobDto.getMaterialId());
+        //need to handle proper empty check and transaction handling
+        Material mat = matById.orElse(null);
+        mat.setStatus("reserved");
+        materialRepo.save(mat);
+
         JobResponse jobResponse = modelMapper().map(savedJob, JobResponse.class);
         log.info("JobResponse values {}",
                 jobResponse.toString());
@@ -54,6 +68,13 @@ public class JobServiceImpl implements JobService {
         Job job = modelMapper().map(jobDto, Job.class);
         job.setStatus("Unreserved");
         Job savedJob = jobRepo.save(job);
+
+        Optional<Material> matById = materialRepo.findById(jobDto.getMaterialId());
+        //need to handle proper empty check and transaction handling
+        Material mat = matById.orElse(null);
+        mat.setStatus("unreserved");
+        materialRepo.save(mat);
+
         JobResponse jobResponse = modelMapper().map(savedJob, JobResponse.class);
         log.info("JobResponse values {}",
                 jobResponse.toString());
